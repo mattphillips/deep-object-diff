fca43fcc18586aae61a3457c96a601c8bedc161b
Matt Phillips <mattphillips1016@gmail.com>
2017-01-25 23:50:30 +0000

    Add re-export of internal default exports

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import diff from './diff';
+import addedDiff from './added';
+import deletedDiff from './deleted';
+import updatedDiff from './updated';
+import detailedDiff from './detailed';
+
+export {
+  addedDiff,
+  diff,
+  deletedDiff,
+  updatedDiff,
+  detailedDiff
+};
