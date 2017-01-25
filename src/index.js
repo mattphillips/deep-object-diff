bcb3b2223092964fc4443b2744fd5fba05b52047
Matt Phillips <mattphillips1016@gmail.com>
2017-01-25 22:20:44 +0000

    Add comments

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ const isObject = o => o != null && typeof o === 'object';
 const isEmpty = o => Object.keys(o).length === 0;
 
 const diff = (lhs, rhs) => {
-  if (lhs === rhs) return {};
+  if (lhs === rhs) return {}; // equal return no diff
 
-  if (!isObject(lhs) || !isObject(rhs)) return rhs;
+  if (!isObject(lhs) || !isObject(rhs)) return rhs; // return updated rhs
 
   const deletedValues = Object.keys(lhs).reduce((acc, key) => {
     return rhs.hasOwnProperty(key) ? acc : { ...acc, [key]: undefined };
   }, {});
 
   return Object.keys(rhs).reduce((acc, key) => {
-    if (!lhs.hasOwnProperty(key)) return { ...acc, [key]: rhs[key] };
+    if (!lhs.hasOwnProperty(key)) return { ...acc, [key]: rhs[key] }; // return added rhs key
 
     const difference = diff(lhs[key], rhs[key]);
 
-    if (isObject(difference) && isEmpty(difference)) return acc;
+    if (isObject(difference) && isEmpty(difference)) return acc; // return no diff
 
-    return { ...acc, [key]: difference };
+    return { ...acc, [key]: difference }; // return updated key
   }, deletedValues);
 };
 
