0a408004c1db579ac0b702f9ded4e953db1694d9
Alexis Côté <alexiscote19@hotmail.com>
2019-04-19 23:13:28 -0400

    Fix the preserve array function with array of objects

--- src/preserveArray.test.js
+++ src/preserveArray.test.js
@@ -2,7 +2,7 @@ import preserveArray from './preserveArray';
 
 describe('.preserveArray', () => {
   test('returns diff with nested objects converted back to arrays when property is deleted', () => {
-    const left = { a: [{ b: ['#', '#', '#', { hello: '' }] }, '#', { c: '', d: ['#', ''] }, '#'] };
+    const left = { a: [{ b: ['#', '#', '#', { hello: '' }, { hi: ''}] }, '#', { c: '', d: ['#', ''] }, '#'] };
     const right = { a: [{ b: ['#', '#', '#', { hello: 'world' }] }, '#', { c: 'hello', d: ['#', 'bob'] }] };
     const diff = {
       a: {
@@ -10,7 +10,8 @@ describe('.preserveArray', () => {
           b: {
             3: {
               hello: 'world'
-            }
+            },
+            4: undefined
           }
         },
         2: {
@@ -31,7 +32,8 @@ describe('.preserveArray', () => {
             'empty',
             {
               hello: 'world'
-            }
+            },
+            undefined
           ]
         },
         'empty',
@@ -42,6 +44,8 @@ describe('.preserveArray', () => {
         undefined
       ]
     };
+
+    // Remove 'empty'
     delete expected.a[0].b[0];
     delete expected.a[0].b[1];
     delete expected.a[0].b[2];
@@ -53,13 +57,16 @@ describe('.preserveArray', () => {
 
   test('returns diff with nested objects converted back to arrays when new property is added', () => {
     const left = { a: [{ b: ['#', '#', '#', { hello: '' }] }, '#', { c: '', d: ['#', ''] }] };
-    const right = { a: [{ b: ['#', '#', '#', { hello: 'world' }] }, '#', { c: 'hello', d: ['#', 'bob'] }, 'foobar'] };
+    const right = { a: [{ b: ['#', '#', '#', { hello: 'world' }, {hi: ''}] }, '#', { c: 'hello', d: ['#', 'bob'] }, 'foobar'] };
     const diff = {
       a: {
         0: {
           b: {
             3: {
               hello: 'world'
+            },
+            4: {
+              hi: ''
             }
           }
         },
@@ -81,6 +88,9 @@ describe('.preserveArray', () => {
             'empty',
             {
               hello: 'world'
+            },
+            {
+              hi: ''
             }
           ]
         },
@@ -92,6 +102,8 @@ describe('.preserveArray', () => {
         'foobar'
       ]
     };
+
+    // Remove 'empty'
     delete expected.a[0].b[0];
     delete expected.a[0].b[1];
     delete expected.a[0].b[2];
