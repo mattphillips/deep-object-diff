a76d39a1f5cc488c9af1057aa2c1db8576cf9bf2
Matt Phillips <matt@mattphillips.io>
2022-01-25 17:20:44 +0000

    Fix broken imports

--- src/diff.js
+++ src/diff.js
@@ -1,4 +1,4 @@
-import { isDate, isEmptyObject, isObject, hasOwnProperty } from '../src/utils';
+import { isDate, isEmptyObject, isObject, hasOwnProperty } from './utils';
 
 const diff = (lhs, rhs) => {
   if (lhs === rhs) return {}; // equal return no diff
